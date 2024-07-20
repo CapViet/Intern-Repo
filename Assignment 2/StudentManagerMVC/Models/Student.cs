ea009160b854bbcc768a8278f26b0c2000e5b6d5
Nguyen Duc Kien Trung <teo171103@gmail.com>
2024-07-20 22:31:37 +0700

    Update Assignment 2

---
+++ Assignment 2/StudentManagerMVC/Models/Student.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace StudentManagerMVC.Models
+{
+    public class Student
+    {
+        public int ID { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [DataType(DataType.Date)]
+        [Display(Name = "Birthday")]
+        public DateTime DateOfBirth { get; set; }
+
+        [Required]
+        [StringLength(10)]
+        [Display(Name = "Gender")]
+        public string Gender { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        [Display(Name = "Birthplace")]
+        public string PlaceOfBirth { get; set; }
+
+        [Required]
+        [StringLength(20)]
+        public string Mobile { get; set; }
+
+        [Display(Name = "Is Graduated?")]
+        public bool IsGraduated { get; set; }
+
+        [Display(Name = "Full Name")]
+        public string FullName => $"{FirstName} {LastName}";
+    }
+}
---
+++ Assignment 2/StudentManagerMVC/Models/ViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace StudentManagerMVC.Models
+{
+    public class ViewModel
+    {
+        public class StudentFilterViewModel
+        {
+            public string FilterField { get; set; }
+            public string FilterCriteria { get; set; }
+            public string FilterValue { get; set; }
+            public List<Student> Students { get; set; }
+        }
+
+    }
+}
